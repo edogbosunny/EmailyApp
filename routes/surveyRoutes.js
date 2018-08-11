0bf5f078ec54e1015df5091ec3e0cec3cc89e14c
edogbosunny <edogbosunday@gmail.com>
2018-08-11 03:13:25 +0100

    changes made to survey routes

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin");
 const requireCredits = require("../middlewares/requireCredits");
+const Mailer = require("../services/Mailer");
+const surveyTemplate = require("../services/emailTemplates/surveyTemplates");
 
 const Survey = mongoose.model("surveys");
 
@@ -10,7 +12,17 @@ module.exports = app => {
     const survey = new Survey({
       title,
       subject,
-      body
+      body,
+      recipients: recipients.split(",").map(email => {
+        return { email: email };
+        _;
+      }),
+      _user: req.user.id,
+      dateSent: Date.now
     });
+
+    //Nice to send our email from this function
+    const mailer = new Mailer(survey, surveyTemplate(survey));
+    mailer.send();
   });
 };
